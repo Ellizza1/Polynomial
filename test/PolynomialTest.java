201e4951509c8645104c61cc6763a7cbd2d97c7e
emil <kazan.emil@mail.ru>
2026-03-04 21:50:42 +0300

    add tests

---
+++ test/PolynomialTest.java
@@ -0,0 +1,52 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class PolynomialTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void copyList() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testHashCode() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPower() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void plus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void minus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void times() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTimes() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void div() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calc() {
+    }
+}
